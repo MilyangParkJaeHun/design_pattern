d85326db17d17b4b4ce263c8d019d0e6586835be
MilyangParkJaeHun <jh9277@naver.com>
2021-10-20 21:32:39 +0900

    Create Composite pattern : python version

---
+++ Composite/python/Composite.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+"""
+common operation 을 선언하는 base Component 클래스
+common operation 은 모든 composition 요소(Leaf, Composite)에서 사용된다.
+"""
+class Component(ABC):
+    @property
+    def parent(self) -> Component:
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent: Component):
+        self._parent = parent
+
+    def add(self, component: Component) -> None:
+        pass
+
+    def remove(self, component: Component) -> None:
+        pass
+
+    """
+    클라이언트 코드에서 구성 요소가 자식을 가질 수 있는지 여부를 파악할 수 있는 메서드를 제공할 수 있습니다.
+    """
+    def is_composite(self) -> bool:
+        return False
+
+    @abstractmethod
+    def operation(self) -> str:
+        pass
+
+
+"""
+보통 Leaf 객체가 실질적인 작업을 하는 반면,
+Composite 객체는 그들의 sub-componets 에게 작업을 위임합니다.
+"""
+class Leaf(Component):
+    def operation(self) -> str:
+        return "Leaf"
+
+
+class Composite(Component):
+    def __init__(self) -> None:
+        self._children: List[Component] = []
+
+    def add(self, component: Component) -> None:
+        self._children.append(component)
+        component.parent = self
+
+    def remove(self, component: Component) -> None:
+        self._children.remove(component)
+        component.parent = None
+
+    def is_composite(self) -> bool:
+        return True
+
+    def operation(self) -> str:
+        results = []
+        for child in self._children:
+            results.append(child.operation())
+        return f"Branch({'+'.join(results)})"
+
+
+def client_code(component: Component) -> None:
+    print(f"RESULT: {component.operation()}", end="")
+
+
+def client_code2(component1: Component, component2: Component) -> None:
+    if component1.is_composite():
+        component1.add(component2)
+
+    print(f"RESULT: {component1.operation()}", end="")
+
+
+if __name__ == "__main__":
+    # This way the client code can support the simple leaf components...
+    simple = Leaf()
+    print("Client: I've got a simple component:")
+    client_code(simple)
+    print("\n")
+
+    # ...as well as the complex composites.
+    tree = Composite()
+
+    branch1 = Composite()
+    branch1.add(Leaf())
+    branch1.add(Leaf())
+
+    branch2 = Composite()
+    branch2.add(Leaf())
+
+    tree.add(branch1)
+    tree.add(branch2)
+
+    print("Client: Now I've got a composite tree:")
+    client_code(tree)
+    print("\n")
+
+    print("Client: I don't need to check the components classes even when managing the tree:")
+    client_code2(tree, simple)
